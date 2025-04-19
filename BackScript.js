487e3474d26de509f792ad71e7f2797b6387b9c7
Nikhil singh <nikhilsingh1696@gmail.com>
2025-04-20 02:44:57 +0530

    updated

--- BackScript.js
+++ BackScript.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 const app= express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 app.get("/",function(req, res){
-    res.sendFile(path.join(__dirname,"public","login.html"))
+    res.sendFile(path.join(__dirname,"login.html"))
+});
+app.get("/accessGrant",function(req, res){
+    res.sendFile(path.join(__dirname,"acessGrant.html"))
 });
 app.get("/main",function(req, res){
-    res.sendFile(path.join(__dirname,"public","index.html"))
+    res.sendFile(path.join(__dirname,"index.html"))
 });
-// app.get("/profile",function(req, res){
-//     res.sendFile(path.join(__dirname,"public","login.html"))
-// });
 app.listen(3000,()=>{
     console.log("Server runningt https://localhost:3000");
 });
